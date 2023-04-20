ef4d433ad10bdbc975c041339a6dd7ffabfb0727
Adam Sałański <adam.salanski1234@gmail.com>
2023-04-20 13:25:25 +0200

    utworzenie gałęzi XYZ2NEU, zaimportowanie biblioteki numpy i math oraz skopiowanie skryptu na transformacje XYZ2NEU

--- skrypt.py
+++ skrypt.py
@@ -1,1 +1,13 @@
-a
+import numpy as np
+from math import *
+
+def Rneu(f,l): # f to fi l to lambda
+    R = np.array([[-np.sin(f) * np.cos(l),-np.sin(l),np.cos(f) * np.cos(l)],
+                  [-np.sin(f) * np.sin(l),np.cos(l),np.cos(f) * np.sin(l)],
+                  [np.cos(f), 0 ,np.sin(f)]])
+    return(R)
+
+#zmiana XYZ na neu
+def XYZ2neu(dX,f,l):
+    R = Rneu(f,l)
+    return(R.T @ dX)
